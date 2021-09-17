dcbdda6a299a395f4c370be5e15090269a0feb62
Vladislav Mikheev <slav009@yandex.ru>
2021-09-17 11:25:51 +0300

    edit spaces

--- search_server_project/request_queue.h
+++ search_server_project/request_queue.h
@@ -28,16 +28,16 @@ private:
 };
 
 template <typename DocumentPredicate>
-    std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-    	requests_.push_back({raw_query, search_server_.FindTopDocuments(raw_query, document_predicate)});
-    	if(requests_.back().search_result.empty()){
-    		++empty_requests_;
-    	}
-		if(requests_.size() > min_in_day_) {
-			if(requests_.front().search_result.empty()){
-				--empty_requests_;
-			}
-			requests_.pop_front();
-		}
-		return requests_.back().search_result;
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    requests_.push_back({raw_query, search_server_.FindTopDocuments(raw_query, document_predicate)});
+    if(requests_.back().search_result.empty()){
+        ++empty_requests_;
     }
+    if(requests_.size() > min_in_day_) {
+        if(requests_.front().search_result.empty()){
+            --empty_requests_;
+        }
+        requests_.pop_front();
+    }
+    return requests_.back().search_result;
+}
